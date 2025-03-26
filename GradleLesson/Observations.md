75145642b0a7f2f50c567d104fefa6c96451daef
Satyam Prajapati <satyamprajapati2k3@gmail.com>
2025-03-26 12:38:28 +0530

    Making http calls with okhttp

---
+++ GradleLesson/Observations.md
@@ -0,0 +1,7 @@
+## Learnings
+
+After adding dependencies in `build.gradle` run following command to download it.
+```sh
+gradle --refresh-dependencies
+```
+
--- GradleLesson/src/main/java/org/mrstm/Main.java
+++ GradleLesson/src/main/java/org/mrstm/Main.java
@@ -1,17 +1,29 @@
 package org.mrstm;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://fakestoreapi.com/products";
+
+        Request req = new Request.Builder()
+                .url(url)
+                .build();
+
+        try(Response response = client.newCall(req).execute()){
+            if(!response.isSuccessful()){
+                System.out.println("Something went wrong" + response.message());
+            }
+
+            System.out.println(response.body().string());
+        }catch (IOException ex){
+            ex.printStackTrace();
         }
     }
 }
